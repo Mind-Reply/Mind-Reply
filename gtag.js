ef0e0d4bb239bc26950455d06eea5e4f18440e34
angellllkr-eng <angellllkr@gmail.com>
2026-05-18 09:50:59 +0300

    Add Google Tag Manager script to gtag.js
    Signed-off-by: angellllkr-eng <angellllkr@gmail.com>

---
+++ gtag.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-HPPGQ3G0GW"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-HPPGQ3G0GW');
+</script>
