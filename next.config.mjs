44af37c7d1fffabb5a9208dd5d8ee1c2fcc7c8ee
Angel K <angellllkr@gmail.com>
2026-05-30 12:28:54 +0300

    Update next.config.mjs
    Signed-off-by: Angel K <angellllkr@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverActions: true
-  }
-};
+next.config.mjs
+package.json
+tsconfig.json
 
-export default nextConfig;
